e37ff7f55d439c032cb5633a20fcb2b2da713905
saicharanruka <saicharanruka@gmail.com>
2025-12-29 15:13:13 +0530

    Added procedural map generation

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import tcod
 
 from engine import Engine
 from entity import Entity
-from game_map import GameMap
+from proc_gen import generate_dungeon
 from input_handlers import EventHandler
 
 
@@ -12,6 +12,10 @@ def main():
 
     map_width = 80
     map_height = 45
+
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
 
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
@@ -33,7 +37,14 @@ def main():
     )
     entities = {player, npc}
 
-    game_map = GameMap(map_width, map_height)
+    game_map = generate_dungeon(
+        max_rooms=max_rooms,
+        room_min_size=room_min_size,
+        room_max_size=room_max_size,
+        map_width=map_width,
+        map_height=map_height,
+        player=player,
+    )
 
     engine = Engine(
         entities=entities, event_handler=event_handler, player=player, game_map=game_map
---
+++ proc_gen.py
@@ -0,0 +1,113 @@
+import tcod
+import random
+from typing import Iterator, Tuple, List
+
+from game_map import GameMap
+import tile_types
+
+# if TYPE_CHECKING:
+from entity import Entity
+
+
+class RectangularRoom:
+    def __init__(self, x: int, y: int, width: int, height: int) -> None:
+        self.x1 = x
+        self.x2 = x + width
+
+        self.y1 = y
+        self.y2 = y + height
+
+    @property
+    def center(self) -> Tuple[int, int]:
+        center_x = int((self.x1 + self.x2) / 2)
+        center_y = int((self.y1 + self.y2) / 2)
+
+        return center_x, center_y
+
+    @property
+    def inner(self) -> Tuple[slice, slice]:
+        """Returns the inner area of this room as a 2D array index"""
+        return slice(self.x1 + 1, self.x2), slice(self.y1 + 1, self.y2)
+
+    def intersects(self, other) -> bool:
+        """Returns True if this room overlaps with another room"""
+        return (
+            self.x1 <= other.x2
+            and self.x2 >= other.x1
+            and self.y1 <= other.y2
+            and self.y2 >= other.y1
+        )
+
+
+# def generate_dungeon(map_width: int, map_height: int) -> GameMap:
+#     dungeon = GameMap(map_width, map_height)
+
+#     room1 = RectangularRoom(x=20, y=10, width=10, height=15)
+#     room2 = RectangularRoom(x=35, y=15, width=10, height=15)
+
+#     dungeon.tiles[room1.inner] = tile_types.floor
+#     dungeon.tiles[room2.inner] = tile_types.floor
+
+#     for x, y in tunnel_between(room2.center, room1.center):
+#         dungeon.tiles[x, y] = tile_types.floor
+
+#     return dungeon
+
+
+def generate_dungeon(
+    max_rooms: int,
+    room_min_size: int,
+    room_max_size: int,
+    map_width: int,
+    map_height: int,
+    player: Entity,
+) -> GameMap:
+    """Generate a new dungeon map."""
+    dungeon = GameMap(map_width, map_height)
+    rooms: List[RectangularRoom] = []
+
+    for r in range(max_rooms):
+        room_width = random.randint(room_min_size, room_max_size)
+        room_height = random.randint(room_min_size, room_max_size)
+
+        # random x and y coordinates
+        x = random.randint(0, dungeon.width - room_width - 1)
+        y = random.randint(0, dungeon.height - room_height - 1)
+        # ----
+
+        new_room = RectangularRoom(x, y, room_width, room_height)
+
+        if any(new_room.intersects(other_room) for other_room in rooms):
+            continue
+
+        dungeon.tiles[new_room.inner] = tile_types.floor
+
+        if len(rooms) == 0:
+            player.x, player.y = new_room.center
+        else:
+            for x, y in tunnel_between(rooms[-1].center, new_room.center):
+                dungeon.tiles[x, y] = tile_types.floor
+
+        rooms.append(new_room)
+
+    return dungeon
+
+
+def tunnel_between(
+    start: Tuple[int, int], end: Tuple[int, int]
+) -> Iterator[Tuple[int, int]]:
+    """Return an L-shaped tunnel between these two points"""
+    x1, y1 = start
+    x2, y2 = end
+
+    if random.random() < 0.5:
+        # Move horizontally then vertically
+        corner_x, corner_y = x2, y1
+    else:
+        corner_x, corner_y = x1, y2
+
+    # Generate the coordinates
+    for x, y in tcod.los.bresenham((x1, y1), (corner_x, corner_y)).tolist():
+        yield x, y
+    for x, y in tcod.los.bresenham((corner_x, corner_y), (x2, y2)).tolist():
+        yield x, y
